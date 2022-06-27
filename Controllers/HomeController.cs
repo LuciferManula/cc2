978a0325dc74d88f8e24a1767cbec6642a0ac5c5
Michael <23063073+rushan-mike@users.noreply.github.com>
2022-06-27 16:01:05 +0530

    db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,7 +25,7 @@ public class HomeController : Controller
     }
 
     // [Authorize]
-    public IActionResult Claims()
+    public IActionResult Users()
     {
         return View();
     }
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,9 +1,11 @@
 namespace CC2.Models;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
     public class Users
     {
         [Key]
         public int? Id { get; set; }
-        public string? Name { get; set; }
+        public string? UserName { get; set; }
     }
